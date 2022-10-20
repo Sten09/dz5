704ce26cd8cf29c7874b5c2964cbe54532e4b976
Sten09 <sten111982@gmail.com9>
2022-10-20 16:55:03 +0300

    dz5

---
+++ file34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+//Console.Clear();
+int[] numbers = new int[4];
+
+void EvenArray(int[] array, int min, int max)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void Array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++ )
+    {
+    Console.Write(array[i] + " ");
+    }
+  Console.WriteLine();
+}
+
+int QuantityPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++ )
+    {
+    if (array[i] % 2 == 1)
+    {
+      quantity++;
+    }
+    }
+  return quantity;
+}
+EvenArray(numbers, 100, 1000);
+Array(numbers);
+Console.WriteLine();
+
+int quantity = QuantityPositive(numbers);
+Console.WriteLine($"Чётных чисел в массиве: {quantity}");
---
+++ file38/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] arrayRealNumbers = new double[5];
+Random rnd = new Random();
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = rnd.Next(1, 5);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+    if (minNumber > arrayRealNumbers[i])
+    {
+    minNumber = arrayRealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+  Console.WriteLine($"\n разница между максимальными и минимальными элементами: {decision}");
